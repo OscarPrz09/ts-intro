79add986080515831442eb5e84caed1d598b68d3
OscarPrz09 <oscarprz2609@gmail.com>
2022-09-19 10:55:00 -0500

    comit numero 1

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,11 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = 'Oscar Perez';
+let hp: number | string = 20;
+let estaVivo: boolean = true;
+
+hp = 'Veinte';
+
+console.log(nombre, 'tiene', hp, 'años y esta vivo',estaVivo);
